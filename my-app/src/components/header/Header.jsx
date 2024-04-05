c66d13d9ec2f94aabd0bd780f0d0b7bb860dae01
hat3rrr <maxim19062004@mail.ru>
2024-04-05 15:30:13 +0300

    create react

---
+++ my-app/src/components/header/Header.jsx
@@ -0,0 +1,20 @@
+const Header = () => {
+    return (
+        <div>
+
+            <header>
+                <h1>
+                    Дадада
+                </h1>
+
+        
+
+        </header>
+            
+        </div>
+
+    );
+
+}
+
+export default Header
