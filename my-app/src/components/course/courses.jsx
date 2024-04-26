a66bfd19461a02a915996980cf0bc6469ad9bd37
hat3rrr <maxim19062004@mail.ru>
2024-04-26 09:41:02 +0300

    lol

--- my-app/src/components/course/courses.jsx
+++ my-app/src/components/course/courses.jsx
@@ -1,16 +1,24 @@
-import styles from './courses.module.scss';
-const courses = ({course}) => {
+import styles from './Course.module.scss';
+// import { useState } from "react";
+const Course = (props) => {
+    const {course, deleteCourse} = props;
+    const handleDelete = (id) => {
+        deleteCourse(id);
+    }
+    
     // const {courses} = props;
     return (
-        <div className ={styles.courses}>
+        <div className ={styles.Course}>
             <h3>Название курса: {course.title}</h3>
             <p>Описание: {course.description}</p>
-            <p>Кол-во часов: {course.hours}</p>
+            {course.hours < 101 ? <p className ={styles.item__hard}> Кол-во часов: {course.hours}</p>: <p> Кол-во часов: {course.hours}</p> }
             <img src={course.img} alt="University" />
+            <button onClick ={()=> handleDelete(course.id)}>Удалить</button>
+            <button>Лайкнуть</button>
 
 
 
         </div>
     )
 }
-export default courses;
+export default Course;
--- my-app/src/components/courseslist/courseslist.jsx
+++ my-app/src/components/courseslist/courseslist.jsx
@@ -1,19 +1,39 @@
-import Courses from "../course/courses";
+import Course from "../course/courses";
 import styles from './courseslist.module.scss';
 import data from '../../data.json';
+import { useEffect, useState } from "react";
 
-const courseslist = () => {
-    // console.log(data)
+const Courseslist = () => {
+    const [datacourses, setDataCourses] = useState([]);
+    useEffect(()=> {
+        setDataCourses(data);
+},[])
+    const [visibleCount, setVisiblecount]= useState(3);
+    const deleteCourse = (id) =>{
+        const updateCourses = datacourses.filter((item)=> item.id!==id);
+        setDataCourses(updateCourses);
+    }
+    // const changeLike = (id) => {
+    //     const updateLike = datacourses.
+    // }
+    const handleVisiblecount =() =>{
+        setVisiblecount(visibleCount+3);}
     return(
+        
         <div className ={styles.courseslist}>
             {/* <h2></h2> */}
             {/* <p>flsalfasl</p> */}
-            {data.map((item) => 
+            {datacourses.slice(0,visibleCount).map((item) => 
                 <div key= {item.id}>
-                <Courses course={item} />
+                <Course course={item} deleteCourse = {deleteCourse} />
                 </div>
 )}
+{visibleCount < datacourses.length && (
+    <button onClick= {handleVisiblecount}className={styles.btn}>Еще</button>
+)}
+{/* <button onClick={handleCounter}> click {counter}</button> */}
         </div>
     )
 }
-export default courseslist;
+
+export default Courseslist;
