2f2f6553b4c73179bc855e2d2d7bb1d92c894e15
hat3rrr <maxim19062004@mail.ru>
2024-04-27 18:38:53 +0300

    hwon2

--- my-app/src/components/courseslist/courseslistApi.jsx
+++ my-app/src/components/courseslist/courseslistApi.jsx
@@ -1,42 +1,77 @@
-import styles from './courseslist.module.scss';
+import styles from './courseslistApi.module.scss';
 import { useEffect, useState } from "react";
 
-const API_KEY = "6844bc1469dc4e75a12521d13850a2f9"; // замените на свой ключ API
+const CourseslistApi = () => {
+  const [users, setUsers] = useState([]);
+  const [posts, setPosts] = useState([]);
 
-const CourseslistApi = () =>
-{
-    const [matches, setMatches] = useState([]);
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then((json) => {
+        setUsers(json);
+        Promise.all(json.map(user => fetch(`https://jsonplaceholder.typicode.com/posts?userId=${user.id}`)))
+          .then(responses => Promise.all(responses.map(response => response.json())))
+          .then((postsList) => {
+            const allPosts = postsList.flat();
+            setPosts(allPosts);
+          });
+      });
+  }, []);
 
-    useEffect (() => {
-        fetch(`https://api.football-data.org/v2/matches?status=FINISHED`, {
-            headers: {
-                "X-Auth-Token": API_KEY
-            }
-        })
-            .then(response => response.json())
-            .then((json) =>
-            {
-                console.log(json);
-                setMatches(json.matches);
-            });
-    }, [])
+  const deletePost = (id) => {
+    const updatedPosts = posts.filter((post) => post.id !== id);
+    setPosts(updatedPosts);
+  };
 
-    return (
-        <>
-            <h1> Football Matches</h1>
-            <div >
-            {matches.map((match) => (
-                <div key={match.id} >
-                <h3> {match.homeTeam.name} - {match.awayTeam.name}</h3>
-                <p> {match.score.fullTime.homeTeam} - {match.score.fullTime.awayTeam}</p>
-                </div>
-      ))}  
+  const toggleLike = (id) => {
+    const updatedPosts = posts.map((post) => {
+      if (post.id === id) {
+        return { ...post, liked: !post.liked };
+      }
+      return post;
+    });
+    setPosts(updatedPosts);
+  };
+
+  return (
+    <>
+      <h1>Данные из API</h1>
+      <div className={styles.container}>
+        {users.map((user) => (
+          <div key={user.id} className={styles.user}>
+            <h3>{user.name}</h3>
+            <p>Email: {user.email}</p>
+            <h4>Their Posts:</h4>
+            <ul className={styles.posts}>
+              {posts
+                .filter((post) => post.userId === user.id)
+                .map((post) => (
+                  <li key={post.id} className={styles.post}>
+                    <div className={styles.postContainer}>
+                      <h5>{post.title}</h5>
+                      <p className={styles.postContent}>{post.body}</p>
+                      <button
+                        className={styles.likeButton}
+                        onClick={() => toggleLike(post.id)}
+                      >
+                        {post.liked ? 'Dislike' : 'Like'}
+                      </button>
+                      <button
+                        className={styles.deleteButton}
+                        onClick={() => deletePost(post.id)}
+                      >
+                        Delete
+                      </button>
+                    </div>
+                  </li>
+                ))}
+            </ul>
+          </div>
+        ))}
       </div>
-      
-      
-        
-        
-        </>
-    );
+    </>
+  );
 }
+
 export default CourseslistApi;
