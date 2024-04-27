1d0b88773a5f9ee1bde0b4aea877f1cf8b670931
hat3rrr <maxim19062004@mail.ru>
2024-04-27 18:03:26 +0300

    no

--- my-app/src/components/courseslist/courseslistApi.jsx
+++ my-app/src/components/courseslist/courseslistApi.jsx
@@ -1,27 +1,34 @@
 import styles from './courseslist.module.scss';
 import { useEffect, useState } from "react";
 
+const API_KEY = "6844bc1469dc4e75a12521d13850a2f9"; // замените на свой ключ API
+
 const CourseslistApi = () =>
 {
-    const [courses, setCourses] = useState([]);
+    const [matches, setMatches] = useState([]);
+
     useEffect (() => {
-        fetch('https://jsonplaceholder.typicode.com/todos')
+        fetch(`https://api.football-data.org/v2/matches?status=FINISHED`, {
+            headers: {
+                "X-Auth-Token": API_KEY
+            }
+        })
             .then(response => response.json())
             .then((json) =>
             {
                 console.log(json);
-                setCourses(json);
+                setMatches(json.matches);
             });
     }, [])
 
     return (
         <>
-            <h1> Data from API</h1>
+            <h1> Football Matches</h1>
             <div >
-            {courses.map((item) => (
-                <div key={item.id} >
-                <h3> {item.title}</h3>
-                <p> {item.body}</p>
+            {matches.map((match) => (
+                <div key={match.id} >
+                <h3> {match.homeTeam.name} - {match.awayTeam.name}</h3>
+                <p> {match.score.fullTime.homeTeam} - {match.score.fullTime.awayTeam}</p>
                 </div>
       ))}  
       </div>
