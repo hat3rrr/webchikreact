0867d1a20fbf0f9f99cd0bf18ae6f2fd48070b24
hat3rrr <maxim19062004@mail.ru>
2024-05-08 17:12:19 +0300

    new

--- my-app/src/components/courseslist/courseslistApi.jsx
+++ my-app/src/components/courseslist/courseslistApi.jsx
@@ -1,9 +1,20 @@
 import styles from './courseslistApi.module.scss';
 import { useEffect, useState } from "react";
+import Button from '@mui/material/Button';
+import Card from '@mui/material/Card';
+import CircularProgress from '@mui/material/CircularProgress';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
 
 const CourseslistApi = () => {
   const [users, setUsers] = useState([]);
   const [posts, setPosts] = useState([]);
+  const [textInfo, setTextInfo] = useState([]);
+  const [loaded, setLoaded] = useState(false);
+  const [textFilter, setTextFilter] = useState('');
+  const [ filteredProducts, setFiltredProducts ] = useState([]);
+
+
 
   useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/users')
@@ -15,10 +26,17 @@ const CourseslistApi = () => {
           .then((postsList) => {
             const allPosts = postsList.flat();
             setPosts(allPosts);
+            setLoaded(true);
           });
       });
   }, []);
 
+  useEffect(() => {
+
+    const t = users.filter(item => item.name.includes(textFilter))
+    console.log(t);
+    setFiltredProducts(t);
+  }, [textFilter])
   const deletePost = (id) => {
     const updatedPosts = posts.filter((post) => post.id !== id);
     setPosts(updatedPosts);
@@ -37,8 +55,18 @@ const CourseslistApi = () => {
   return (
     <>
       <h1>Данные из API</h1>
+      {
+        !loaded && <Box >
+        <CircularProgress />
+      </Box>
+      }
+      <p> {textFilter}</p>
+      <TextField id="standard-basic" label="Filter" variant="standard"
+        onChange = {(e) => setTextFilter(e.target.value)} />
       <div className={styles.container}>
-        {users.map((user) => (
+      
+        {(textFilter === '')?
+        users.map((user) => (
           <div key={user.id} className={styles.user}>
             <h3>{user.name}</h3>
             <p>Email: {user.email}</p>
@@ -51,24 +79,54 @@ const CourseslistApi = () => {
                     <div className={styles.postContainer}>
                       <h5>{post.title}</h5>
                       <p className={styles.postContent}>{post.body}</p>
-                      <button
-                        className={styles.likeButton}
+                      <Button variant = "outlined"
                         onClick={() => toggleLike(post.id)}
                       >
                         {post.liked ? 'Dislike' : 'Like'}
-                      </button>
-                      <button
+                        </Button>
+                      <Button variant = "contained"
                         className={styles.deleteButton}
                         onClick={() => deletePost(post.id)}
                       >
                         Delete
-                      </button>
+                      </Button>
                     </div>
                   </li>
                 ))}
             </ul>
           </div>
-        ))}
+        )):
+        filteredProducts.map((user) => (
+          <div key={user.id} className={styles.user}>
+            <h3>{user.name}</h3>
+            <p>Email: {user.email}</p>
+            <h4>Their Posts:</h4>
+            <ul className={styles.posts}>
+              {posts
+                .filter((post) => post.userId === user.id)
+                .map((post) => (
+                  <li key={post.id} className={styles.post}>
+                    <div className={styles.postContainer}>
+                      <h5>{post.title}</h5>
+                      <p className={styles.postContent}>{post.body}</p>
+                      <Button variant = "outlined"
+                        onClick={() => toggleLike(post.id)}
+                      >
+                        {post.liked ? 'Dislike' : 'Like'}
+                        </Button>
+                      <Button variant = "contained"
+                        className={styles.deleteButton}
+                        onClick={() => deletePost(post.id)}
+                      >
+                        Delete
+                      </Button>
+                    </div>
+                  </li>
+                ))}
+            </ul>
+          </div>
+        ))
+        }
       </div>
     </>
   );
